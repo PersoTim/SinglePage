f948be783669f173d368c3a5205443132227c590
tgvdufuture <contact.clowntrone.minipato@gmail.com>
2026-09-24 17:01:31 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+const cards = document.querySelectorAll(".card")
+
+cards.forEach((card) => {
+    const img = card.querySelector("img")
+    const contenu = card.querySelector(".contenu")
+
+    let flipped = false
+
+    card.addEventListener("click", () => {
+        if (!flipped) {
+            img.style.transform = "rotateY(-180deg)"
+            contenu.style.transform = "rotateY(0deg)"
+            flipped = true
+
+            setTimeout(() => {
+                img.style.transform = "rotateY(0deg)"
+                contenu.style.transform = "rotateY(180deg)"
+                flipped = false;
+            }, 5000)
+        } else {
+            img.style.transform = "rotateY(0deg)"
+            contenu.style.transform = "rotateY(180deg)"
+            flipped = false
+        }
+    })
+})
+
+
+
+const uls = document.querySelectorAll("nav ul")
+const lis = document.querySelectorAll("nav ul li")
+
+lis.forEach((li) => {
+    li.addEventListener("mouseenter", () => {
+        lis.forEach((li) => {
+            li.classList.remove("underline", "underline-offset-6")
+        })
+
+        li.classList.add("underline", "underline-offset-6")
+    })
+})
+
+uls.forEach((ul) => {
+    ul.addEventListener("mouseleave", () => {
+        lis.forEach((li) => {
+            if (li.classList.contains("selected")) {
+                li.classList.add("underline", "underline-offset-6")
+            } else {
+                li.classList.remove("underline", "underline-offset-6")
+            }
+        })
+    })
+})
