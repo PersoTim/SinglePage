0bdbefd3e6a98022747d4973026c21b27c063b7f
tgvdufuture <contact.clowntrone.minipato@gmail.com>
2026-09-25 13:49:10 +0200

    changement vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        collections: resolve(__dirname, 'collections.html'),
+      },
+    },
+  },
+})
